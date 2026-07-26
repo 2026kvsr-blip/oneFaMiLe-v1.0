61474497716153060273e8ae30daee977435a69d
2026kvsr-blip <2026kvsr@gmail.com>
2026-07-26 15:40:42 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ Part 1A.3
 
 ===================================== */
 
-
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbwWKppI9nuXOgscF4_XfigRW0xlwaPgSxvFTutBFrgPYb1QwBlBF1snTMxkS-O-kklK3g/exec";
 
@@ -2947,6 +2946,36 @@ class="back-btn">
 `
     );
 
+document.getElementById("savePassCodeBtn").onclick = () => {
+
+    const oldPass =
+    document.getElementById("oldPassCode").value.trim();
+
+    const newPass =
+    document.getElementById("newPassCode").value.trim();
+
+    const confirmPass =
+    document.getElementById("confirmPassCode").value.trim();
+
+    if(oldPass === "" || newPass === "" || confirmPass === ""){
+        alert("Please fill all fields.");
+        return;
+    }
+
+    if(oldPass !== sessionStorage.getItem("passCode")){
+        alert("Old Pass Code is incorrect.");
+        return;
+    }
+
+    if(newPass !== confirmPass){
+        alert("New Pass Code and Confirm Pass Code do not match.");
+        return;
+    }
+
+    alert("Validation Successful");
+
+};
+    
     document.getElementById("changePassBackBtn").onclick = ()=>{
 
     homeBtn.click();
