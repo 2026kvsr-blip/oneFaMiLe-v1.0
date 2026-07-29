38c1f04d33bc34d395b219b79808261fcb57b861
2026kvsr-blip <2026kvsr@gmail.com>
2026-07-29 08:29:04 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -92,6 +93,11 @@ document.getElementById("forgotPassCodeBtn");
 
 const forgotPassCodePage =
 document.getElementById("forgotPassCodePage");
+const sendForgotOTPBtn =
+document.getElementById("sendForgotOTPBtn");
+
+const forgotMobileNo =
+document.getElementById("forgotMobileNo");
 
 const backForgotBtn =
 document.getElementById("backForgotBtn");
