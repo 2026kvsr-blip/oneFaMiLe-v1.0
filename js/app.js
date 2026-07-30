698a60aa88dca1b99eb1e67e6ee1fe1db15427f9
2026kvsr-blip <2026kvsr@gmail.com>
2026-07-30 08:52:47 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
-
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -1446,10 +1445,12 @@ backSignupBtn.onclick = ()=>{
 };
 
 
-signupOTPBtn.onclick = ()=>{
+signupOTPBtn.onclick = async ()=>{
 
-     if(!validateSignup()) return;
-     if(!(await checkSignup())) return;
+    if(!validateSignup()) return;
+
+    if(!(await checkSignup())) return;
+
     otpMode = "signup";
 
     clearOTP();
