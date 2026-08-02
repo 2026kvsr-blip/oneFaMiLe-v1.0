42a3d8eb855f452072294297556cd385317c09aa
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-02 11:27:51 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 oneFaMiLe V1
 Part 1A.3
 ===================================== */
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -199,6 +200,11 @@ const otpSendingMsg =
 document.getElementById("otpSendingMsg");
 const cooldownTimer =
 document.getElementById("cooldownTimer");
+const loginLockMsg =
+document.getElementById("loginLockMsg");
+
+const loginLockTimer =
+document.getElementById("loginLockTimer");
 
 /* ==========================
    LOGIN SECURITY
@@ -504,7 +510,7 @@ function startLoginLock(){
 
     document.getElementById("loginPassCode").disabled = true;
     document.getElementById("loginSubmitBtn").disabled = true;
-
+loginLockMsg.classList.remove("hidden");
     updateLoginLock();
 
     clearInterval(loginLockInterval);
