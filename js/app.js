03edd95255abd2c029a4ef9630e7c669024f2faa
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-02 22:04:27 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 oneFaMiLe V1
 Part 1A.3
 ===================================== */
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -2917,6 +2918,7 @@ const formData = new FormData();
 },3000);          
 }
     }catch(err){
+        hideLoader();
 
         alert("Unable to connect to server.");
         console.log(err);
