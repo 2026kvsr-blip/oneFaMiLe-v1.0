e83db0187d14d5ea16865667caa6e836bef7dd1d
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-03 15:23:14 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -2057,7 +2058,7 @@ async function sendForgotOTP(){
        showMessage(
     "Enter Mobile Number.",
     "warning",
-    5000
+    3000
 );
         forgotMobileNo.focus();
         return;
