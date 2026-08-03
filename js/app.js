958720e5ffaddb9fb7b8e8889c97b98b0e3dcb02
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-03 16:36:49 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
-
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -4202,16 +4201,6 @@ if(toggle){
     };
 
 }
-document.getElementById("verifyPass").onclick = function(){
-
-    showMessage(
-        "Sensitive Pass Code verification will be added in the next step.",
-        "info",
-        3000
-    );
-
-};
-
     
 document.getElementById("verifyPass").onclick = () => {
 
@@ -4222,14 +4211,30 @@ document.getElementById("verifyPass").onclick = () => {
         sessionStorage.getItem("passCode");
 
     if (enteredPass === "") {
-        alert("Enter Pass Code");
-        return;
-    }
+
+    showMessage(
+        "Enter Pass Code.",
+        "warning",
+        3000
+    );
+
+    document.getElementById("reportPassCode").focus();
+
+    return;
+}
 
     if (enteredPass !== savedPass) {
-        alert("Wrong Pass Code");
-        return;
-    }
+
+    showMessage(
+        "Wrong Pass Code.",
+        "warning",
+        3000
+    );
+
+    document.getElementById("reportPassCode").focus();
+
+    return;
+}
 
     reportsLayout(
         moduleName + " " + reportType,
