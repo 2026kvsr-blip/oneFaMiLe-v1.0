71430684c77532e5359d0c603970c8f4953dd2aa
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-04 18:07:58 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -743,7 +744,20 @@ function showScreen(page){
 
 }
     page.classList.remove("hidden");
-   
+   const appTitle =
+document.getElementById("appTitle");
+
+if(page === welcomePage){
+
+    appTitle.textContent =
+    "Welcome to oneFaMiLe";
+
+}else{
+
+    appTitle.textContent =
+    "oneFaMiLe";
+
+}
     window.scrollTo(0,0);
 
 }
