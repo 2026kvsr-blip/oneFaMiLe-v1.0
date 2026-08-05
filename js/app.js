f46c11c2d8a0407c5a25a795797c43ccb609ca32
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-05 11:11:32 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -5623,7 +5624,8 @@ backResetPassCodeBtn.onclick = ()=>{
 /* ======================
 APP START
 ====================== */
-
+sessionStorage.removeItem("user");
+sessionStorage.removeItem("passCode");
 showScreen(welcomePage);
 updateWelcomePage();
 updateMenuIcon();
