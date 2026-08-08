9a606356a45c7ccfefa40bb127b5a6f5b3ca516e
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-08 19:01:08 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -263,7 +264,8 @@ document.getElementById("settingsPage");
 
 const settingsMenuBtn =
 document.getElementById("settingsMenuBtn");
-
+const profileMenuBtn =
+document.getElementById("profileMenuBtn");
 const backSettingsBtn =
 document.getElementById("backSettingsBtn");
 
