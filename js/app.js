fd90cbb261cd82f6810b797a1ae2a4283e6753e4
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-08 15:39:45 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 oneFaMiLe V1
 Part 1A.3
 ===================================== */
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
