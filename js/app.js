0127229771862696ce743713e6f26a9155839000
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-09 12:19:19 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 oneFaMiLe V1
 Part 1A.3
 ===================================== */
+
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -6984,13 +6985,9 @@ user.country = country;
 // BACK
 // =====================================
 
-    document.getElementById("profileEditBackBtn").onclick = ()=>{
+   document.getElementById("profileEditBackBtn").onclick = ()=>{
 
-    profilePage.classList.add("hidden");
-
-    showScreen(dashboard);
-
-    homeBtn.click();
+    document.getElementById("cancelEditProfileBtn").click();
 
 };
 
