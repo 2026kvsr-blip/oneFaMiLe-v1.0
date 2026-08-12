507700c66a248cc6235266ac650e83d847c57802
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-12 21:09:18 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 oneFaMiLe V1
 Part 1A.3
 ===================================== */
-
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -6995,9 +6994,6 @@ otpFormData.append(
 const otpResult =
     await otpResponse.json();
 
-const sendingMsg =
-    document.getElementById(
-        "mobileOtpSendingMsg"
     );
 
 if(sendingMsg){
