68791ab3eb97f172e06800d405e26b7c7438c8c3
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-16 17:03:00 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ oneFaMiLe V1
 Part 1A.3
 ===================================== */
 
-
 /* WELCOME SCREEN */
 const API_URL ="https://script.google.com/macros/s/AKfycbzYblwgxrGFDF2MKhiWLvrlSLdJTIgQoplD0Z2-A_tLmwrdUPWsTqzOF9-txnug4DFLpg/exec";
 let otpMode = "signup";
@@ -8034,6 +8033,21 @@ document
     // =====================================
 
     showEditProfile();
+
+};
+    // =====================================
+// EDIT PROFILE BACK BUTTON
+// =====================================
+
+document
+    .getElementById("profileEditBackBtn")
+    .onclick = ()=>{
+
+    // =====================================
+    // GO TO MY PROFILE
+    // =====================================
+
+    updateProfilePage();
 
 };
 // =====================================
