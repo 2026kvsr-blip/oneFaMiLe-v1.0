7e1d078cdcee0dd88ed5fb4a32414a2993c6b9b9
2026kvsr-blip <2026kvsr@gmail.com>
2026-08-26 17:34:52 +0530

    Update family.js

--- js/family.js
+++ js/family.js
@@ -8,6 +8,7 @@
    F-NAME-RANDOM4
    ===================================== */
 
+
 function generateFamilyId(familyName){
 
     const cleanName =
@@ -8507,7 +8508,7 @@ const serverFamily = {
         new Date().toISOString()
 
 };
-                  
+        }                  
 
 
 localStorage.setItem(
