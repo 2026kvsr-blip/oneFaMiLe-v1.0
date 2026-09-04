771cc55c04b36980db1352e220d553a60e0bbbca
2026kvsr-blip <2026kvsr@gmail.com>
2026-09-04 16:52:01 +0530

    Change button text from 'Create Family Tree' to 'Create Family'

--- js/family.js
+++ js/family.js
@@ -119,7 +119,7 @@ familyBtn.onclick = () => {
                     class="btn-icon">
 
                 <span>
-                    Create Family Tree
+                    Create Family
                 </span>
 
             </button>
