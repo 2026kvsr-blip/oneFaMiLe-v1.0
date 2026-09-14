cfb778bca8aba36f44bdc2a0a6a8f6cfa00d60d2
2026kvsr-blip <2026kvsr@gmail.com>
2026-09-14 23:07:01 +0530

    Update family.js

--- js/family.js
+++ js/family.js
@@ -14528,7 +14528,7 @@ if(zoomInBtn){
            familyTreeZoomFactor =
     Math.min(
         familyTreeZoomFactor + 1,
-        12
+        15
     );
             applyFamilyTreeZoom();
 
@@ -14745,7 +14745,7 @@ if(treeWrapper){
                         Math.max(
                             1,
                             Math.min(
-                                12,
+                                15,
                                 pinchStartZoom *
                                 ratio
                             )
