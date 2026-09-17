7cb8b76b75db6d351c25c01f68157039af3d04fc
2026kvsr-blip <2026kvsr@gmail.com>
2026-09-17 05:39:22 +0530

    Update family.js

--- js/family.js
+++ js/family.js
@@ -1,4 +1,5 @@
 
+
 /* =====================================
    oneFaMiLe
    FAMILY MODULE
@@ -16473,7 +16474,7 @@ function drawFamilyTreeLines(){
 
 
     svg.innerHTML = "";
-
+alignSplitAncestorRoots();
 
     function getPoint(
         element,
@@ -16611,7 +16612,108 @@ const rect =
         );
 
     }
+/* =====================================
+   ALIGN ANCESTOR ROOTS
+   WITH SELECTED + PARTNER
+   ===================================== */
 
+function alignSplitAncestorRoots(){
+
+    const memberTree =
+        canvas.querySelector(
+            ".family-tree-member-ancestor-tree"
+        );
+
+    const partnerTree =
+        canvas.querySelector(
+            ".family-tree-partner-ancestor-tree"
+        );
+
+    const selectedNode =
+        canvas.querySelector(
+            ".tree-selected"
+        );
+
+    const partnerNode =
+        canvas.querySelector(
+            ".tree-partner"
+        );
+
+
+    if(
+        !memberTree ||
+        !partnerTree ||
+        !selectedNode ||
+        !partnerNode
+    ){
+        return;
+    }
+
+
+    /* RESET OLD CORRECTIONS */
+
+    memberTree.style.transform = "";
+    partnerTree.style.transform = "";
+
+
+    const memberRect =
+        memberTree.getBoundingClientRect();
+
+    const partnerTreeRect =
+        partnerTree.getBoundingClientRect();
+
+    const selectedRect =
+        selectedNode.getBoundingClientRect();
+
+    const partnerRect =
+        partnerNode.getBoundingClientRect();
+
+
+    /*
+       Member ancestry RIGHT edge should meet
+       selected member center.
+    */
+
+    const selectedCenterX =
+        selectedRect.left +
+        selectedRect.width / 2;
+
+
+    const memberRootX =
+        memberRect.right;
+
+
+    const memberMoveX =
+        selectedCenterX -
+        memberRootX;
+
+
+    /*
+       Partner ancestry LEFT edge should meet
+       partner center.
+    */
+
+    const partnerCenterX =
+        partnerRect.left +
+        partnerRect.width / 2;
+
+
+    const partnerRootX =
+        partnerTreeRect.left;
+
+
+    const partnerMoveX =
+        partnerCenterX -
+        partnerRootX;
+
+
+    memberTree.style.transform =
+        `translateX(${memberMoveX}px)`;
+
+
+    partnerTree.style.transform =
+        `translateX(${partnerMoveX}px)`;
+}
 
     function connectCouple(
         first,
