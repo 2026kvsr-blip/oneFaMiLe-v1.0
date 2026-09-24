c5a7158c3153e7b49cd74c127eb251e7f1c0718f
2026kvsr-blip <2026kvsr@gmail.com>
2026-09-24 19:44:24 +0530

    Update family.js

--- js/family.js
+++ js/family.js
@@ -1,5 +1,6 @@
 
 
+
 /* =====================================
    oneFaMiLe
    FAMILY MODULE
@@ -12843,31 +12844,7 @@ function fitFamilyTreeToScreen(){
     rightMost -
     leftMost;
 
-console.log(
-    "TREE FIT DEBUG:",
-    {
-        zoomBeforeFit: familyTreeZoom,
-        treeWidth,
-        leftMost,
-        rightMost,
-        leftNode:
-            visibleNodes.reduce(
-                (a, b) =>
-                    a.getBoundingClientRect().left <
-                    b.getBoundingClientRect().left
-                        ? a
-                        : b
-            ).textContent.trim(),
-        rightNode:
-            visibleNodes.reduce(
-                (a, b) =>
-                    a.getBoundingClientRect().right >
-                    b.getBoundingClientRect().right
-                        ? a
-                        : b
-            ).textContent.trim()
-    }
-);
+
 /* =====================================
    ACTUAL TREE VERTICAL BOUNDS
    ===================================== */
